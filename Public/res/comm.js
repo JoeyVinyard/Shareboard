bf038d983f0260fefc4d81055a025c5fde8f9526
JoeyVinyard <jviny98@yahoo.com>
2016-08-26 10:53:37 -0400

    Basic collaborative work available

--- Public/res/comm.js
+++ Public/res/comm.js
@@ -1,7 +1,8 @@
 var server = io();
+var mouseHeld = false;
 var info = {
 	position	:[0,0],
-	color		:genColor()
+	col		:genColor()
 }
 function genColor(){
 	//Generates a random 6 digit number to be used as a color
@@ -11,4 +12,16 @@ function genColor(){
 $(document).mousemove(function(event){
 	info.position[0] = event.pageX;
 	info.position[1] = event.pageY;
+	if(mouseHeld){
+		server.emit('draw', info);
+	}
 });
+$(document).mousedown(function(){
+	mouseHeld = true;
+});
+$(document).mouseup(function(){
+	mouseHeld = false;
+});
+server.on('drawPoint', function(info){
+	drawPoint(info);
+})
--- Public/res/screendraw.js
+++ Public/res/screendraw.js
@@ -8,3 +8,7 @@ function prepCanv(){
 	$("#canv").attr("height",$(window).height());
 	context = canvas.getContext("2d");
 }
+function drawPoint(info){
+	context.fillStyle = info.col;
+	context.fillRect(info.position[0]-1, info.position[1]-1, 2, 2);
+}
